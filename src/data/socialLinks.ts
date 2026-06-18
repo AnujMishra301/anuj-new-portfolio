78164afb256f59f78f18a3442d3a9ff0958e8141
Anuj Kumar Mishra <mishraanuj3011@gmail.com>
2026-06-18 13:15:17 +0530

    Updated resume

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -1,4 +1,4 @@
 export const github = "https://github.com/AnujMishra301"
 export const linkedin = "https://www.linkedin.com/in/anuj-kumar-mishra-085295303/"
-export const resume = "/resume.pdf"
+export const resume = "/resume1.pdf"
 export const email = "mailto:mishraanuj301@gmail.com"
